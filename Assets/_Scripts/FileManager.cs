659371d2ec3f9e975cca3f7c3905fe80821c618f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-28 12:38:31 -0400

    Added Saving and Loading Functionality

--- Assets/_Scripts/FileManager.cs
+++ Assets/_Scripts/FileManager.cs
@@ -3,104 +3,123 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-public class FileManager : MonoBehaviour {
 
-	const string DLL_NAME = "VoxelEnginePlugin";
+public class FileManager : MonoBehaviour
+{
 
-	// File IO
-	[DllImport(DLL_NAME)]
-	private static extern void LoadMap(string filePath);
+    const string DLL_NAME = "VoxelEnginePlugin";
 
-	[DllImport(DLL_NAME)]
-	private static extern void SaveMap(string filePath);
+    // File IO
+    [DllImport(DLL_NAME)]
+    private static extern void LoadMap(string filePath);
 
-	// World Map Accessors
-	[DllImport(DLL_NAME)]
-	private static extern System.IntPtr getBlock();
+    [DllImport(DLL_NAME)]
+    private static extern void SaveMap(string filePath);
 
-	[DllImport(DLL_NAME)]
-	private static extern void setBlock(byte[] blockArray, int width, int height, int depth);
+    // World Map Accessors
+    [DllImport(DLL_NAME)]
+    private static extern System.IntPtr getBlock();
 
-	[DllImport(DLL_NAME)]
-	private static extern int getWidth();
+    [DllImport(DLL_NAME)]
+    private static extern void setBlock(byte[] blockArray, int width, int height, int depth);
 
-	[DllImport(DLL_NAME)]
-	private static extern int getHeight();
+    [DllImport(DLL_NAME)]
+    private static extern int getWidth();
 
-	[DllImport(DLL_NAME)]
-	private static extern int getDepth();
+    [DllImport(DLL_NAME)]
+    private static extern int getHeight();
+
+    [DllImport(DLL_NAME)]
+    private static extern int getDepth();
+
+    //non public declaration
+    public WorldController worldController;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            onLoadMap();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            onSaveMap();
+        }
+    }
+
+    public void onSaveMap()
+    {
+        Debug.Log("Saving Map Data...");
+        int width = worldController.width;
+        int height = worldController.height;
+        int depth = worldController.depth;
+
+        byte[] blocksToSend = new byte[width * height * depth];
+        int count = 0;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < depth; z++)
+                {
+                    blocksToSend[count] = worldController.Blocks[x, y, z];
+                    count++;
+                }
+            }
+        }
+
+        setBlock(blocksToSend, width, height, depth);
+
+
+        SaveMap("Test.map");
+    }
+
+
+    public void onLoadMap()
+    {
+
+        LoadMap("Test.map");
+        Debug.Log("Loading Map Data...");
+        int width = getWidth();
+        int height = getHeight();
+        int depth = getDepth();
+        int size = width * height * depth;
+
+		worldController.RemoveAllBlocksFromWorld();
+		worldController.MakeAllAir();
+        worldController.Blocks = new byte[width, height, depth];
+        worldController.BlockArray = new GameObject[width, height, depth];
 		
-		int width = 64;
-		int height = 32;
-		int depth = 64;
 
-		byte[,,] blocks = new byte[width, height, depth];
+        byte[] newblocks = new byte[size];
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				for (int z = 0; z < depth; z++)
-				{
-					blocks[x, y, z] = (byte)Random.Range(0, 3);
-				}
-			}
-		}
-
-		
-		byte[] blocksToSend = new byte[width*height*depth];
-		int count = 0;
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				for (int z = 0; z < depth; z++)
-				{
-					blocksToSend[count] = blocks[x ,y ,z];
-					count++;
-				}
-			}
-		}
-		
-		setBlock(blocksToSend, width, height, depth);
+        // get map data from saved file
+        Marshal.Copy(getBlock(), newblocks, 0, size);
 
 
-		SaveMap("Test.map");
-		
+        // Block Regeneration
+        int count = 0;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < depth; z++)
+                {
+                    worldController.Blocks[x, y, z] = newblocks[count];
+                    count++;
+                }
+            }
+        }
 
-
-		// load and get position
-		
-
-		/* 
-		LoadMap("Test.map");
-		int width = getWidth();
-		int height = getHeight();
-		int depth = getDepth();
-		int size = width * height * depth;
-
-		Debug.Log("Width:  " + width);
-		Debug.Log("Height: " + height);
-		Debug.Log("Depth:  " + depth);
-
-		 
-		byte[] newblocks = new byte[size];
-
-		
-		Marshal.Copy(getBlock(), newblocks,0, size);
-		*/
-
-		
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        worldController.Regenerate(true);
+    }
 }
